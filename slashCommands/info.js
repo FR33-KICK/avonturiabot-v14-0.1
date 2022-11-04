a6cc3c73f39d915f5d75286bf9a10991157c1302
FR33-KICK <80514181+FR33-KICK@users.noreply.github.com>
2022-11-04 20:09:16 +0100

    Update info.js

--- slashCommands/info.js
+++ slashCommands/info.js
@@ -8,8 +8,11 @@ module.exports = {
 	async execute(client,interaction) {
 
 		var botEmbed = new EmbedBuilder()
-		.setTitle("_AvonturiaParkMC_ is een online custom pretpark in het spel Minecraft. Het park bestaat uit vijf themagebieden met unieke attracties. Zien we jou binnenkort ook een ritje maken in een van de attracties? Of ben jij bij een van de grote evenementen?  :confetti_ball: ")
+		.setTitle("AvonturiaParkMC | Informatie")
 		.setColor("#992D22")
+        .addFields(
+			{name: '_AvonturiaParkMC_ is een online custom pretpark in het spel Minecraft. Het park bestaat uit vijf themagebieden met unieke attracties. Zien we jou binnenkort ook een ritje maken in een van de attracties? Of ben jij bij een van de grote evenementen?  :confetti_ball: ', value: `test`}
+        )
         .setFooter({ text: 'AvonturiaParkMC | Informatie', iconURL: 'https://i.imgur.com/E1irn4t.png'});
 
 	await interaction.reply({embeds: [botEmbed], ephemeral: true});
